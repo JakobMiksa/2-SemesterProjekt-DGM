8b6b82478506d33f399f6a3fc8ef02ac4e63d467
oscarvk123 <10700464@ucn.dk>
2026-05-06 13:15:54 +0200

    Model lag helt done

--- src/dgm/model/ReservedOrder.java
+++ src/dgm/model/ReservedOrder.java
@@ -5,40 +5,52 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.ArrayList;
 
-public class SaleOrder {
+public class ReservedOrder {
 	private LocalDate date;
+	private LocalDate expiryDate;
 	private String paymentMethod;
 	private BigDecimal total;
-	private List<SaleOrderLine> orderLines;
+	private Customer customer;
+	private List<ReservedOrderLine> orderLines; 
 	
-	public SaleOrder() {
+	public ReservedOrder() {
 		this.date = LocalDate.now();
+		this.expiryDate = LocalDate.now().plusDays(3);
 		this.paymentMethod = null;
 		this.total = BigDecimal.ZERO;
-	    this.orderLines = new ArrayList<>();
-		}
-
-	public void addLine(SaleOrderLine line) {
+		this.customer = null;
+		this.orderLines = new ArrayList<>();
+	}
+	
+	public void addLine(ReservedOrderLine line) {
 	    this.orderLines.add(line);
-	}		
+	}
 	
 	public BigDecimal calculateTotal() {
         BigDecimal calculatedTotal = BigDecimal.ZERO;
 
-        for (SaleOrderLine line : orderLines) {
+        for (ReservedOrderLine line : orderLines) {
             calculatedTotal = calculatedTotal.add(line.getSubtotal());
-        }
-
+    }
+        
         total = calculatedTotal;
         return total;
     }
-	
+
 	public LocalDate getDate() {
 		return date;
 	}
 
 	public void setDate(LocalDate date) {
 		this.date = date;
+	}
+
+	public LocalDate getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(LocalDate expiryDate) {
+		this.expiryDate = expiryDate;
 	}
 
 	public String getPaymentMethod() {
@@ -57,15 +69,20 @@ public class SaleOrder {
 		this.total = total;
 	}
 
-	public List<SaleOrderLine> getOrderLines() {
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public List<ReservedOrderLine> getOrderLines() {
 		return orderLines;
 	}
 
-	public void setOrderLines(List<SaleOrderLine> orderLines) {
+	public void setOrderLines(List<ReservedOrderLine> orderLines) {
 		this.orderLines = orderLines;
 	}
 	
-	
 }
-	
-
--- src/dgm/model/ReservedOrderLine.java
+++ src/dgm/model/ReservedOrderLine.java
@@ -7,38 +7,38 @@ public class ReservedOrderLine {
 	private BigDecimal unitPrice;
 	private Product product;
 	
-public ReservedOrderLine(Product product, int quantity, BigDecimal unitPrice) {
-	this.product = product;
-	this.quantity = quantity;
-	this.unitPrice = unitPrice;
-}
+	public ReservedOrderLine(Product product, int quantity, BigDecimal unitPrice) {
+		this.product = product;
+		this.quantity = quantity;
+		this.unitPrice = unitPrice;
+	}
 
-public BigDecimal getSubtotal() {
-	 BigDecimal res = unitPrice.multiply(BigDecimal.valueOf(quantity));
-    return res;
-}
+	public BigDecimal getSubtotal() {
+		BigDecimal res = unitPrice.multiply(BigDecimal.valueOf(quantity));
+		return res;
+	}
 
-public int getQuantity() {
-	return quantity;
-}
+	public int getQuantity() {
+		return quantity;
+	}
 
-public void setQuantity(int quantity) {
-	this.quantity = quantity;
-}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 
-public BigDecimal getUnitPrice() {
-	return unitPrice;
-}
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
 
-public void setUnitPrice(BigDecimal unitPrice) {
-	this.unitPrice = unitPrice;
-}
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
 
-public Product getProduct() {
-	return product;
-}
+	public Product getProduct() {
+		return product;
+	}
 
-public void setProduct(Product product) {
-	this.product = product;
-}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
 }
