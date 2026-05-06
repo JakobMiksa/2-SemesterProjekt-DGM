cf64514e9ef1a0cb7eca81b2824b0d13f2f34545
denrigtigemarvin <10481788@ucn.dk>
2026-05-06 11:51:38 +0200

    StockItemDB Skelet

---
+++ src/dgm/database/StockItemDB.java
@@ -0,0 +1,29 @@
+package dgm.database;
+
+public class StockItemDB {
+	private DBConnection connection; 
+
+	public StockItemDB() {
+	
+	}
+	
+	public StockItem findAvailableStock(int productNumber) { 
+		
+	}
+	
+	public void decreaseAvailableQty(int productNumber, int quantity) {
+		
+	}
+	
+	private StockItem buildObject(ResultSet rs) {
+		
+	}
+	
+	public DBConnection getConnection() {
+		
+	}
+	
+	public void setConnection(DBConnection connection) {
+		
+	}
+}
